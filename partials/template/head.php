bb66d1cdaf076e1cf5ac72e3304b1bc5302f77a9
Andrea <b.a.benvenuti3@gmail.com>
2021-05-05 16:29:47 +0200

    css con sass

--- partials/template/head.php
+++ partials/template/head.php
@@ -13,8 +13,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     <!-- /FontAwesome -->
     <!-- css -->
-    <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="stylesheet" href="dist/app.css"> -->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="dist/app.css">
     <!-- /css -->
     <!-- favicon -->
     <link rel="icon" type="image/png" href="https://static.vecteezy.com/system/resources/previews/001/206/478/non_2x/music-flat-icon-vinyl-record-png.png" />
